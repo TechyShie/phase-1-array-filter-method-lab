c42c29663eb1fa4fc7bab55cb637bd0692128a66
TechyShie <susanshie316@gmail.com>
2025-06-20 11:30:21 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(
+        (driver) => driver.toLowerCase() === name.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, name){
+      return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(name.toLowerCase())
+  );
+
+}
+
+function matchName(drivers, nameToMatch) {
+  return drivers.filter(driver => driver.name === nameToMatch);
+}
